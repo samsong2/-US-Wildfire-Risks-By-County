30fe0bc56e8f950b7ef821dddd34a64c86429d52
Sam <samsong974@gmail.com>
2020-11-11 18:18:18 -0800

    drew county lines

--- src/CountyMap.js
+++ src/CountyMap.js
@@ -3,19 +3,31 @@
   
 // Draws the base map with just the counties
 async function draw_map(){
-    var us = await d3.json("./data/tl_2019_us_county_topojson_simplified.json", function (error, us) {
+    var width = 1000
+    var height = 500
+    var svg = d3.select(".container").select("svg")
+    var projection = d3.geoAlbersUsa()
+
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+
+    var us = await d3.json("./data/tl_2019_us_county_basic.json", function (error, us) {
         if (error) 
             return console.error(error);
         console.log(us);
     });
 
+    var counties = topojson.feature(us, us.objects.tl_2019_us_county);
+    var county_features = topojson.feature(us, us.objects.tl_2019_us_county).features
 
-    var svg = d3.select(".container").select("svg")
+    svg.selectAll(".counties")
+        .data(county_features)
+        .enter().append("path")
+        .attr("class", function(d) { return "COUNTYNS " + d.properties.COUNTYNS; })
+        .attr("d", path);
     
-    svg.append("path")
-        .datum(topojson.feature(us, us.objects.tl_2019_us_county))
-        .attr("d", d3.geoPath().projection(d3.geoMercator()))
-
 }
 
 function init(){
