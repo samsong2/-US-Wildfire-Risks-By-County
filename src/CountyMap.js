04753a4d6239fcf37181dc8118416a002f1f9466
Sam <samsong974@gmail.com>
2020-11-15 17:37:03 -0800

    albers to focus mainland

--- src/CountyMap.js
+++ src/CountyMap.js
@@ -6,7 +6,7 @@ async function draw_map(){
     var width = 1000
     var height = 500
     var svg = d3.select(".container").select("svg")
-    var projection = d3.geoAlbersUsa()
+    var projection = d3.geoAlbers()
 
 
     var path = d3.geoPath()
@@ -19,17 +19,17 @@ async function draw_map(){
         console.log(us);
     });
 
-    var counties = topojson.feature(us, us.objects.tl_2019_us_county);
     var county_features = topojson.feature(us, us.objects.tl_2019_us_county).features
+
+    //var county_features = topojson.feature(us, us.objects.counties).features
 
     svg.selectAll(".counties")
         .data(county_features)
         .enter().append("path")
-        .attr("id", function(d) { return  d.properties.COUNTYNS; })
-        .attr("name", function(d){ return  d.properties.NAME;})
-        .attr("state", function(d){ return d.properties.STATEFP})
-        .attr("geoid", function(d){ return d.properties.GEOID})
         .attr("class", "county")
+        .attr("ID", function(d) { return d.properties.GEOID;})
+        .attr("Name", function(d) { return d.properties.NAME;})
+        .attr("State", function(d) { return d.properties.STATEFP;})
         .attr("d", path);
     
 }
